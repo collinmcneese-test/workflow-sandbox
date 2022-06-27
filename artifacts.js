ff6e2267c47161089f1b5ce7d73262fb1327167a
CollinM <collinmcneese@github.com>
2022-06-27 13:06:12 -0500

    artifacts
    Signed-off-by: CollinM <collinmcneese@github.com>

--- artifacts.js
+++ artifacts.js
@@ -4,13 +4,15 @@ const octokit = new Octokit();
 const [owner, repo] = process.env.GITHUB_REPOSITORY.split("/");
 const workflowId = process.env.GITHUB_RUN_ID;
 
-await octokit.actions.listWorkflowRunArtifacts({
+octokit.actions.listWorkflowRunArtifacts({
   owner,
   repo,
   run_id: workflowId
 }).then(response => {
   const artifacts = response.data.artifacts;
   artifacts.forEach(artifact => {
+    console.log('Cleaning up artifact:', artifact.name);
+
     octokit.actions.deleteArtifact({
       owner,
       repo,
